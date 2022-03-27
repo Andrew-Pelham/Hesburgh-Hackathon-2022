41543ab453ea902cd8b39031f722c337796dbe81
Matthew Spadafore <mspadafo@nd.edu>
2022-03-26 23:37:17 -0400

    Added Regex/Broken and File Output

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import tornado.ioloop
 import tornado.web
 import json
 import os
+import random
+import re
 
 
 #Function to load into the json
@@ -19,16 +21,39 @@ def compBuilder(theDict, buildList, theUsed):
             if buildList[x][key]["item"] == ' ':
                 for dkey in theDict: 
                     if theDict[dkey]["type"] == key:
-                        if theDict[dkey]["item"] not in theUsed:
-                            buildList[x][key] = theDict[dkey]
-                            theUsed.append(theDict[dkey]["item"])
-                            del theDict[dkey]
-                            break 
-    
+                        if theDict[dkey]["broken"] == False:
+                            if theDict[dkey]["item"] not in theUsed:
+                                buildList[x][key] = theDict[dkey]
+                                theUsed.append(theDict[dkey]["item"])
+                                del theDict[dkey]
+                                break 
+
+#makes a list of broken objects
+#searches through common synonyms for broken
+def makeBroken(theDict):
+    theRegex = re.compile(r'fix|broke|crack|repair|busted|not work|does not work|fixing')
+    for key in theDict:
+        if re.search(theRegex, theDict[key]["notes"].lower()):
+            theDict[key]["broken"] = True;
+            
+
+#makes an output ticket
+#The ticket has properties to contact the owners of the parts
+def makeOutput(theDict, buildList):
+    fileName = "buildTicket.txt"
+    with open(fileName, 'w') as outFile:
+        for x in range(3):
+            if  {"item": " "} not in buildList[x].values():
+                outFile.write(f'Build {x+1}:\n')
+                outFile.write(f'Desktop: {buildList[x]["desktop"]["item"]} from {buildList[x]["desktop"]["name"]} at {buildList[x]["desktop"]["dorm"]} with email of {buildList[x]["desktop"]["email"]}\n')
+                outFile.write(f'Mouse: {buildList[x]["mouse"]["item"]} from {buildList[x]["mouse"]["name"]} at {buildList[x]["mouse"]["dorm"]} with email of {buildList[x]["mouse"]["email"]}\n')
+                outFile.write(f'Keyboard: {buildList[x]["keyboard"]["item"]} from {buildList[x]["keyboard"]["name"]} at {buildList[x]["keyboard"]["dorm"]} with email of {buildList[x]["keyboard"]["email"]}\n')
+                outFile.write(f'Monitor: {buildList[x]["monitor"]["item"]} from {buildList[x]["monitor"]["name"]} at {buildList[x]["monitor"]["dorm"]} with email of {buildList[x]["monitor"]["email"]}\n\n')
+
 
 class MainHandler(tornado.web.RequestHandler):
     def post(self):
-        self.render("fifIndex.html", dict=theDict, theBuilds=theBuilds, theUsed=theUsed)
+        self.render("sevIndex.html", dict=theDict, theBuilds=theBuilds, theUsed=theUsed)
         
         #Gathers user entries to put in the dictionary
         theName = self.get_argument('name', '')
@@ -38,9 +63,11 @@ class MainHandler(tornado.web.RequestHandler):
         theType = self.get_argument('type', '').lower()
         theDorm = self.get_argument('dorm', '')
 
+        #if the type is not entered by default it is misc
         if not theType:
             theType = 'misc'
 
+        #puts the new entry in the dictionary
         if (theItem):
             newPart = {}
             newPart["name"] = theName
@@ -49,10 +76,14 @@ class MainHandler(tornado.web.RequestHandler):
             newPart["notes"] = theNotes
             newPart["type"] = theType
             newPart["dorm"] = theDorm
+            newPart["broken"] = False
             theDict[newPart["item"]] = newPart
 
+        #builds computers and saves output
+        makeBroken(theDict)
         compBuilder(theDict, theBuilds, theUsed)
         saveJson(theDict)
+        makeOutput(theDict, theBuilds)
         
         #compBuilder(theDict, theBuilds, theUsed)
 #<center>
@@ -66,7 +97,7 @@ class MainHandler(tornado.web.RequestHandler):
             #with open("theJson.txt", "w") as dataFile:
                 #json.dump(theDict, dataFile)
     def get(self):       
-        self.render("fifIndex.html", dict=theDict, theBuilds=theBuilds, theUsed=theUsed)
+        self.render("sevIndex.html", dict=theDict, theBuilds=theBuilds, theUsed=theUsed)
 
 
         
@@ -75,21 +106,23 @@ def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
     ])
-    '''
+    
 class make_app(tornado.web.Application):
     def __init__(self):
         handlers = [
                 (r"/", MainHandler),
                     ]
-        tornado.web.Application.__init__(self, handlers, **settings)
+        tornado.web.Application.__init__(self, handlers, **settings) '''
 
 if __name__ == "__main__":
+
+    #first gets the dictionary from the json
     data = open("sampleParts.json")
     theDict = json.load(data)
 
     #load settings for Tornado to import the proper css/images
     settings = {
-    "static_path": os.path.join(os.path.dirname(__file__), "static"),
+   "static_path": os.path.join(os.path.dirname(__file__), "static"),
     }
 
     #Set up the web app
@@ -102,6 +135,7 @@ if __name__ == "__main__":
     #initialize the builds as empty
     theBuilds = []
     theUsed = []
+    theBroken = []
     for x in range(3):
         emptyDict = {}
         plainDict = {"item": " "}
@@ -112,7 +146,10 @@ if __name__ == "__main__":
         theBuilds.append(emptyDict)
 
     #Build computers
+    #makeBroken(theDict, theBroken)
     compBuilder(theDict, theBuilds, theUsed)
+    makeOutput(theDict, theBuilds)
 
+    #main web page loop
     app.listen(9999)
     tornado.ioloop.IOLoop.current().start()
